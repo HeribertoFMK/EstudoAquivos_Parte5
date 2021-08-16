ff58e6e172efbdcc0499dded4ee567215043bbbb
Heriberto FMK <hfmk2015@gmail.com>
2021-08-16 11:58:49 +0900

    Incio do estudo sobre Directory, DirectoryInfo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EstudoAquivos_Parte5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
